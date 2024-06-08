778de43b0aa92642cb03eb8a963b8c442aed1cb9
Jose Gonzalez <gonzalezj231@gmail.com>
2024-06-08 11:05:13 -0400

    rest Template

--- src/main/java/com/example/MS_A/ProductAController.java
+++ src/main/java/com/example/MS_A/ProductAController.java
@@ -1,6 +1,6 @@
 package com.example.MS_A;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +9,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@AllArgsConstructor
 public class ProductAController {
-    @Autowired
     private ProductServ productService;
 
     @GetMapping("/lista")
--- src/main/java/com/example/MS_A/ProductServ.java
+++ src/main/java/com/example/MS_A/ProductServ.java
@@ -1,5 +1,6 @@
 package com.example.MS_A;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class ProductServ {
     @Autowired
     private RestTemplate restTemplate;
@@ -15,6 +17,7 @@ public class ProductServ {
     public List<ProductResponse> getAllB() {
         String url = "http://localhost:8086/api/lista";
         ProductResponse[] response = restTemplate.getForObject(url, ProductResponse[].class);
+        assert response != null;
         return Arrays.asList(response);
     }
 }
